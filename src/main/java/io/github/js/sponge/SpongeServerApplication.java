bfded3def0dbf6ef300ed57aa9d376c3dac57464
jaesungahn91 <jaesungahn91@gmail.com>
2023-12-04 16:46:15 +0900

    init

---
+++ src/main/java/io/github/js/sponge/SpongeServerApplication.java
@@ -0,0 +1,13 @@
+package io.github.js.sponge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpongeServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpongeServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/github/js/sponge/SpongeServerApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.js.sponge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpongeServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
